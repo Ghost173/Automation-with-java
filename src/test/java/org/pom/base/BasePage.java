e47163824b41742d21f309adfc0399be3cab140b
ghost173 <jpirakas007@gmail.com>
2021-12-23 22:06:22 +0530

    first commit

---
+++ src/test/java/org/pom/base/BasePage.java
@@ -0,0 +1,12 @@
+package org.pom.base;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    private WebDriver driver;
+
+    public BasePage (WebDriver driver) {
+        this.driver =driver;
+    }
+}
