5dc22a0aa0e66bf9ff9f6e2f45c6915a1e6cde91
ghost173 <jpirakas007@gmail.com>
2021-12-25 14:09:06 +0530

    webdriver updated

--- src/test/java/org/pom/factory/DriverManager.java
+++ src/test/java/org/pom/factory/DriverManager.java
@@ -1,5 +1,6 @@
 package org.pom.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,7 +9,8 @@ import java.time.Duration;
 public class DriverManager {
 
     public WebDriver initializeDriver() {
-        System.setProperty("webdriver.chrome.driver", "/home/prakash/IdeaProjects/Askohmautomation_dec/driver/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "/home/prakash/IdeaProjects/Askohmautomation_dec/driver/chromedriver");
+        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
